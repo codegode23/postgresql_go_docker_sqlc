fe74af7b2cdc2bc202b626ea2a3a2753062f93fa
codegode23 <jasperakabua020@gmail.com>
2023-06-16 17:32:37 +0000

    figuring out super tokens

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/supertokens/supertokens-golang/recipe/passwordless"
+	"github.com/supertokens/supertokens-golang/recipe/passwordless/plessmodels"
+	"github.com/supertokens/supertokens-golang/recipe/session"
+	"github.com/supertokens/supertokens-golang/supertokens"
+)
+
+func main() {
+	apiBasePath := "/auth"
+	websiteBasePath := "/webpath"
+	err := supertokens.Init(supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			// https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+			ConnectionURI: "https://try.supertokens.com",
+			// APIKey: <API_KEY(if configured)>,
+		},
+		AppInfo: supertokens.AppInfo{
+			AppName:         "supertokpractice",
+			APIDomain:       "http://localhost:8000",
+			WebsiteDomain:   "http://localhost:8000",
+			APIBasePath:     &apiBasePath,
+			WebsiteBasePath: &websiteBasePath,
+		},
+		RecipeList: []supertokens.Recipe{
+			passwordless.Init(plessmodels.TypeInput{
+				FlowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+				ContactMethodEmailOrPhone: plessmodels.ContactMethodEmailOrPhoneConfig{
+					Enabled: true,
+				},
+			}),
+			session.Init(nil), // initializes session features
+		},
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
