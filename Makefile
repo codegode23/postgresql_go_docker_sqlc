bee432bbfae5d5037fce8246d85fb082f00bca47
codegode23 <jasperakabua020@gmail.com>
2023-06-17 01:42:18 +0000

    middleware figured out and running super token with docker

---
+++ Makefile
@@ -0,0 +1,4 @@
+docker.auth.core: |
+			docker run \
+			-p 3567:3567 -d registry.supertokens.io/supertokens/supertokens-postgresql:5.0
+.PHONY: docker.auth.core
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/supertokens/supertokens-golang/recipe/passwordless"
 	"github.com/supertokens/supertokens-golang/recipe/passwordless/plessmodels"
 	"github.com/supertokens/supertokens-golang/recipe/session"
@@ -8,20 +11,20 @@ import (
 )
 
 func main() {
-	apiBasePath := "/auth"
-	websiteBasePath := "/webpath"
+	// apiBasePath := "/auth"
+	// websiteBasePath := "/webpath"
 	err := supertokens.Init(supertokens.TypeInput{
 		Supertokens: &supertokens.ConnectionInfo{
 			// https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
-			ConnectionURI: "https://try.supertokens.com",
+			ConnectionURI: "http://localhost:3567",
 			// APIKey: <API_KEY(if configured)>,
 		},
 		AppInfo: supertokens.AppInfo{
-			AppName:         "supertokpractice",
-			APIDomain:       "http://localhost:8000",
-			WebsiteDomain:   "http://localhost:8000",
-			APIBasePath:     &apiBasePath,
-			WebsiteBasePath: &websiteBasePath,
+			AppName:       "supertokpractice",
+			APIDomain:     "http://localhost:8000",
+			WebsiteDomain: "http://localhost:3000",
+			// APIBasePath:     &apiBasePath,
+			// WebsiteBasePath: &websiteBasePath,
 		},
 		RecipeList: []supertokens.Recipe{
 			passwordless.Init(plessmodels.TypeInput{
@@ -37,4 +40,28 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
+	http.ListenAndServe("0.0.0.0:8000", corsMiddleware(
+		supertokens.Middleware(http.HandlerFunc(func(rw http.ResponseWriter,
+			r *http.Request) {
+			// TODO: Handle your APIs..
+
+		}))))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(response http.ResponseWriter, r *http.Request) {
+		response.Header().Set("Access-Control-Allow-Origin", "http://localhost:8000")
+		response.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == "OPTIONS" {
+			// we add content-type + other headers used by SuperTokens
+			response.Header().Set("Access-Control-Allow-Headers",
+				strings.Join(append([]string{"Content-Type"},
+					supertokens.GetAllCORSHeaders()...), ","))
+			response.Header().Set("Access-Control-Allow-Methods", "*")
+			response.Write([]byte(""))
+		} else {
+			next.ServeHTTP(response, r)
+		}
+	})
 }
